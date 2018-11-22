acd7e4cd55f0ac9116e81b941b41470fd3aaf21a
unknown <fede.est4@gmail.com>
2018-11-22 17:47:57 -0300

    imagenes y algunos estilos

--- app/src/main/java/com/example/federico/appandroid/IndexActivity.java
+++ app/src/main/java/com/example/federico/appandroid/IndexActivity.java
@@ -16,6 +16,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -45,6 +46,7 @@ public class IndexActivity extends AppCompatActivity {
     private NavigationView navigationView;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imgImagen;
+    private RecyclerView postList;
 
     private FusedLocationProviderClient mfuedLocation;
 
